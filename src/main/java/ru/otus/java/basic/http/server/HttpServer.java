78c1fbb77875e2fdc328a769028dc9b98902488f
Fateev Dmitriy <FateevDV@digtp.com>
2025-05-11 18:03:43 +0300

    ДЗ урока 32: Добавлен пулл потоков. Рефакторинг кода.

--- src/main/java/ru/otus/java/basic/http/server/HttpServer.java
+++ src/main/java/ru/otus/java/basic/http/server/HttpServer.java
@@ -2,6 +2,8 @@ package ru.otus.java.basic.http.server;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class HttpServer {
     private final int port;
@@ -10,17 +12,20 @@ public class HttpServer {
         this.port = port;
     }
 
-    public void start() {
+    public void start(int numThreads) {
+        ExecutorService executorService = Executors.newFixedThreadPool(numThreads);
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             serverSocket.setReuseAddress(true);
             System.out.println("Сервер запущен на порту: " + port);
             while (true) {
                 Socket clientSocket = serverSocket.accept();                 
                 ClientHandler clientHandler = new ClientHandler(clientSocket);
-                new Thread(clientHandler).start();
+                executorService.execute(clientHandler);
             }
         } catch (Exception e) {
             e.printStackTrace();
-        } 
+        } finally {
+            executorService.shutdown();
+        }
     }
 }
