e080d66418da4804313013fa5a38d0d42c650f8b
Fateev Dmitriy <fateev@inbox.ru>
2025-05-25 23:52:41 +0300

    CourseWork: Рефакторинг.

--- src/main/java/ru/otus/java/basic/http/server/Application.java
+++ src/main/java/ru/otus/java/basic/http/server/Application.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.Logger;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.util.InputMismatchException;
 import java.util.Properties;
 import java.util.Scanner;
 
@@ -27,62 +28,56 @@ public class Application {
     }
 
     public static void main(String[] args) {
-        Properties propsRepository;
-        propsRepository = readPropertiesFile(PROPERTY_FILE_NAME);
-        if (propsRepository == null) { return; }
-        try {
-            defaultServerPort = Integer.parseInt(propsRepository.getProperty("defaultServerPort").trim());
-            minServerPort = Integer.parseInt(propsRepository.getProperty("minServerPort").trim());
-            maxServerPort = Integer.parseInt(propsRepository.getProperty("maxServerPort").trim());
-            threadPoolSize = Integer.parseInt(propsRepository.getProperty("threadPoolSize").trim());
-            httpRequestSizeLimit = Integer.parseInt(propsRepository.getProperty("httpRequestSizeLimit").trim());
-            httpResponseSizeLimit = Integer.parseInt(propsRepository.getProperty("httpResponseSizeLimit").trim());
-        } catch (NumberFormatException e) {
-            logger.error("Ошибка преобразования строкового значения свойства из файла свойств в целое число. {}", e.getMessage());
-            e.printStackTrace();
-        }
         int serverPort = defaultServerPort;
         try {
-            serverPort = askServerPort();
+            initParamsFromProperties(PROPERTY_FILE_NAME);
+            serverPort = askUserForServerPort();
+            new HttpServer(serverPort).start(threadPoolSize);
         } catch (IOException e) {
-            logger.error("{} Будет использованы параметр из файла свойств.", e.getMessage());
+            logger.error("Файл свойств '{}' не найден или не содержит данных. {}", PROPERTY_FILE_NAME, e.getMessage());
+            e.printStackTrace();
         }
-        new HttpServer(serverPort).start(threadPoolSize);
     }
 
-    private static int askServerPort() throws IOException {
+    private static void initParamsFromProperties(String propertyFileName) throws IOException {
+        Properties properties = null;
+        File propertyFile = new File(propertyFileName);
+        if (!propertyFile.exists() || propertyFile.isDirectory()) {
+            throw new IOException("Файл '" + propertyFileName + "' не существует.");
+        }
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(propertyFileName), StandardCharsets.UTF_8))) {
+            properties = new Properties();
+            properties.load(in);
+            if (!properties.containsKey("defaultServerPort")
+                    || !properties.containsKey("minServerPort")
+                    || !properties.containsKey("maxServerPort")
+                    || !properties.containsKey("threadPoolSize")
+                    || !properties.containsKey("httpRequestSizeLimit")
+                    || !properties.containsKey("httpResponseSizeLimit")) {
+                throw new IOException("Файл '" + propertyFileName + "' не содержит всех необходимых свойств.");
+            }
+            defaultServerPort = Integer.parseInt(properties.getProperty("defaultServerPort").trim());
+            minServerPort = Integer.parseInt(properties.getProperty("minServerPort").trim());
+            maxServerPort = Integer.parseInt(properties.getProperty("maxServerPort").trim());
+            threadPoolSize = Integer.parseInt(properties.getProperty("threadPoolSize").trim());
+            httpRequestSizeLimit = Integer.parseInt(properties.getProperty("httpRequestSizeLimit").trim());
+            httpResponseSizeLimit = Integer.parseInt(properties.getProperty("httpResponseSizeLimit").trim());
+        }
+    }
+
+    private static int askUserForServerPort() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите номер порта на котором будет работать запускаемый HTTP-сервер." +
                 System.lineSeparator() + "Номер порта должен быть в диапазоне " + minServerPort + "-" + maxServerPort +":");
         String userInputStr = scanner.nextLine();
         if (userInputStr.isEmpty()) {
-            throw new IOException("Не задан номер порта сервера.");
+            throw new InputMismatchException("Не задан номер порта сервера.");
         }
         int serverPort;
-        try {
-            serverPort = Integer.parseInt (userInputStr.trim());
-        } catch (NumberFormatException e) {
-            throw new IOException("Введённый номер порта не является числом.");
-        }
+        serverPort = Integer.parseInt (userInputStr.trim());
         if (serverPort < minServerPort || serverPort > maxServerPort) {
-            throw new IOException("Введённый номер порта не входит в разрешенный диапазон номеров портов " + minServerPort + "-" + maxServerPort +".");
+            throw new InputMismatchException("Введённый номер порта не входит в разрешенный диапазон номеров портов " + minServerPort + "-" + maxServerPort +".");
         }
         return serverPort;
     }
-
-    private static Properties readPropertiesFile(String fileName) {
-        Properties properties = null;
-        try (BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(fileName), StandardCharsets.UTF_8))) {
-            properties = new Properties();
-            properties.load(in);
-        } catch (FileNotFoundException e) {
-            logger.error("Файл '{}' не найден. Невозможно прочитать параметры программы из файла свойств. ", fileName);
-            e.printStackTrace();
-        } catch (IOException e) {
-            logger.error("Возникло исключение при чтении файла свойств программы. ");
-            e.printStackTrace();
-        }
-        return properties;
-    }
-
 }
