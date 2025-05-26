dcb49ded0cc961e39e57f34645f9472159acb9e2
Fateev Dmitriy <fateev@inbox.ru>
2025-05-27 02:21:20 +0300

    Removed .idea files

---
+++ src/main/java/ru/otus/java/basic/http/server/exceptions/NotAcceptableResponse.java
@@ -0,0 +1,21 @@
+package ru.otus.java.basic.http.server.exceptions;
+
+public class NotAcceptableResponse extends RuntimeException {
+    private final String code;
+    private final String message;
+
+    public String getCode() {
+        return code;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public NotAcceptableResponse(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
+
